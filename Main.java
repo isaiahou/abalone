b2674cf9af3dac386446ddf3a090e89a7716f9e2
Isaiah <isaiahou@berkeley.edu>
2022-05-16 03:46:40 -0700

    Added a setting argument to main

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board("default");
+        String setting = args[0];
+        Board board = new Board(setting);
         Scanner scanner = new Scanner(System.in);
         System.out.println("next move");
         String userInput = scanner.nextLine();
